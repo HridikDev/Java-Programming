eb34ed812ede8909a9abe536eab279783cbb3199
Hridik Dev <hridikori@gmail.com>
2025-08-21 15:37:24 +0530

    Second Commit

---
+++ Hello(java).java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class s{
+    public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+System.out.println("HELLO");
+String name=s.nextLine();
+System.out.println("Hello"+name);
+    }
+}
---
+++ testaddition.java
@@ -0,0 +1,12 @@
+import mathoperation.*;
+
+public class testaddition{
+
+	public static void main(String[] args){
+
+	addition a=new addition();
+
+	System.out.println("the addition is "+a.add(20,30));
+}
+
+}
